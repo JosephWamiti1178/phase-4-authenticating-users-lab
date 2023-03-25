18e0eb60f4a84493c58be975a9aefba0856ffd8b
Joseph <wamiti8711@gmail.com>
2023-03-25 06:25:37 -0400

    authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+#     Find a user in the database using the user id from the session hash
+# Return the user as a JSON object
+def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  end
+  
+end
